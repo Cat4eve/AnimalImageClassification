e2ae30faa2b240a521c1abc1e915dc9fc255bf12
Cat4eve <71460804+Cat4eve@users.noreply.github.com>
2023-06-13 18:40:12 +0400

    Update

--- classification_neural_network.py
+++ classification_neural_network.py
@@ -3,21 +3,17 @@ import matplotlib as plt
 
 class Animal_Classification_Neural_Network:
     def __init__(self):
-        self.model : tf.keras.Sequential = tf.keras.Sequential()
-
+        self.model : tf.keras.Model = tf.keras.applications.mobilenet_v2.MobileNetV2(
+            classes=10
+        )
 
     def __call__(self, X, y=None, training=True):
         if not training: return self.model(X)
 
-        MobileNetV2 : tf.keras.Model = tf.keras.applications.mobilenet_v2.MobileNetV2(
-            classes=10
-        )
-
-        for layer in MobileNetV2.layers:
-            self.model.add(layer)
+        # train
 
         return self.__call__(X, y, False)
 
     def include_images(self):
-        self.train_dataset = tf.keras.utils.image_dataset_from_directory('./train', shuffle=True)
+        self.train = tf.keras.utils.image_dataset_from_directory('./train', shuffle=True)
         self.test = tf.keras.utils.image_dataset_from_directory('./test', shuffle=True)
